9497f699cc9cab8f301d6734c1482e47b86513bf
neto <84919858+jnreis03@users.noreply.github.com>
2025-04-12 22:19:18 -0300

    11 linhas

---
+++ trabalho.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    printf("OI\n");
+    printf("Tchau\n");
+    printf("OI\n");
+    printf("Tchau\n");
+    printf("OI\n");
+    printf("Tchau\n");
+    printf("OI\n");
+    printf("Tchau\n");
+}
